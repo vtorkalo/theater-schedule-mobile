be4c01f944b1c2ba221912aa08de78abafa6740d
Vladyslav Mamai <41128386+vladmamai@users.noreply.github.com>
2019-01-28 19:54:23 +0200

    LVNETZZZ-14. Install Redux store to React project (#3)
    * Created project template
    
    * Changed project's titles
    
    * Changed files' names
    
    * Moved project's files to root folder
    
    * Redux store is installed

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import defaultReducer from './Reducers/Reducer'; 
+
+const store = createStore(defaultReducer);
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Theater Schedule!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome to Theater Schedule!</Text>
+        </View>
+      </Provider>
     );
   }
 }
---
+++ Reducers/Reducer.js
@@ -0,0 +1,13 @@
+//import { } from 'theater-schedule-mobile/Actions/ActionTypes'
+
+const initialState = {
+
+};
+
+export default function defaultReducer(state = initialState, action) {    
+    switch (action.type) {
+       
+        default:
+            return state;
+    }
+}
