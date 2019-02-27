d54361b359203be6202ab0dc40912802109b44a5
dashakrashchenko <dashakrashenko@gmail.com>
2019-02-28 00:48:52 +0200

    Added image gallery to performance details screen

---
+++ Screens/PerformanceDetailsComponents/ImageGallery.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ScrollView, StyleSheet, FlatList, Text } from 'react-native';
+
+export default ImageGallery = (props) => {
+    return (
+        <ScrollView>
+            <Text style={styles.text}>{props.galleryTitle}</Text>
+            <FlatList
+                horizontal
+                data={props.images}
+                keyExtractor={(item) => item.uri}
+                renderItem={props.showImage}
+            />
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 20,
+        justifyContent: 'center',
+        textAlign: 'center',
+    }
+})
