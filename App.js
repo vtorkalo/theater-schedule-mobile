b9cc2a40dc406f9b0f4cd517b3456a48fb0d531a
Yura <yurapav99@gmail.com>
2019-03-01 16:07:49 +0200

    add switch between base64 and url type of image

--- App.js
+++ App.js
@@ -9,15 +9,13 @@ import message from "./Reducers/messageReducer";
 import { middleware } from "./Navigation/Navigator";
 import { translations } from "./Localization/translations";
 import I18n, { i18nState } from "redux-i18n";
-import Navigator from "./Navigation/Navigator";
 import sliderReducer from "./Reducers/SliderReducer";
-import watchListReducer from "./Reducers/WatchListReducer";
 import thunk from "redux-thunk";
 import { loadSettings } from "./Actions/settingsActions";
 import DeviceInfo from "react-native-device-info";
 import performanceReducer from "./Reducers/PerformanceReducer";
 import AppNavigator from "./AppNavigatorComponent";
-import { setAppReady } from "./Actions/AppActions/AppActionCreators";
+import  wishListReducer  from "./Reducers/WishListReducer";
 import registerForNotification from "./services/pushNotification";
 import { Notifications } from "expo";
 
