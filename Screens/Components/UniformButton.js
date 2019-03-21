a62b005ae41fb1c8adafcd183094884b003ce8c2
Yura <yurapav99@gmail.com>
2019-03-21 12:44:55 +0200

    fix conflict

--- Screens/Components/UniformButton.js
+++ Screens/Components/UniformButton.js
@@ -5,14 +5,8 @@ import { Button, Text } from 'native-base';
 
 export default UniformButton = (props) => {
     const styles = StyleSheet.create({
-<<<<<<< HEAD
         button: {
             backgroundColor: '#7154b8',
-=======
-        button: {    
-            backgroundColor: '#7154b8',
-            ...props.style
->>>>>>> 13186921805912b3424e8ca95e54f6c6af004800
         },
         buttonText: {
             color: '#fff',
@@ -22,15 +16,8 @@ export default UniformButton = (props) => {
 
     });
     return (
-<<<<<<< HEAD
         <Button rounded style={[styles.button, { ...props.style }]} onPress={props.onPress} >
             <Text uppercase={false} style={[styles.buttonText, { ...props.buttonText }]}>{props.text}</Text>
         </Button>
-=======
-            <Button rounded style={styles.button} onPress={props.onPress} >
-                <Text uppercase={false} style={styles.buttonText}>{props.text}</Text>
-            </Button>
->>>>>>> 13186921805912b3424e8ca95e54f6c6af004800
-
     );
 }
