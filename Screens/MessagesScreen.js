4bcfaa4f4d63331eac003bfb85833f6938f3ff64
yuriyhoroshko <yuriy.horoshkolv@gmail.com>
2019-07-17 12:38:20 +0300

    added little styles to messages, added messages fetching

--- Screens/MessagesScreen.js
+++ Screens/MessagesScreen.js
@@ -2,34 +2,122 @@ import LocalizeComponent from "../Localization/LocalizedComponent";
 import React from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { connect } from 'react-redux';
-import { StyleSheet, View, Dimensions, Image, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text,AsyncStorage,FlatList } from 'react-native';
 import DrawerMenuIcon from 'TheaterSchedule/Navigation/DrawerMenuIcon';
-import { Container, Content, Button } from 'native-base';
+import { Container, Button } from 'native-base';
+import { BallIndicator } from 'react-native-indicators';
 import BASE_URL from 'TheaterSchedule/BaseURLDubbing'
 
 class MessagesScreen extends LocalizeComponent
 {
+    constructor(props)
+    {
+        super(props);
+        this.state=
+        {
+            privateMessages:[{}],
+            publicMessages:[{}],
+            currentPublic:true,
+            isLoaded:false
+        }
+    }
     static navigationOptions = ({ screenProps }) => {
         return {
             drawerIcon: (<MaterialCommunityIcons name="message" size={25} />),
             title: screenProps.MessagesScreenTitle,
         };
     };
+    async fetchMessages()
+    {
+        if (this.state.currentPublic)
+        {
+            let messages;
+            fetch(`${BASE_URL}AdminsPost`).then(response=>{
+                return response.json();
+                //console.log(response);
+               // console.log(messages);
+            }).then((msgs)=>{
+                console.log(msgs);
+                this.setState({publicMessages:msgs, isLoaded:true});
+            })
+        }
+
+    }
     componentDidMount() {
-       // this.props.fetchAllMessages(BASE_URL + 'api/performance')
+       //this.props.fetchAllMessages(BASE_URL + 'api/performance')
+       this.fetchMessages();
     }
     render(){
+        if(!this.state.isLoaded){
+            return(
+                <Container style={{ flex: 1,backgroundColor:'#BFD0D670' }}>
+                   
+                    <DrawerMenuIcon
+                            onPressMenuIcon={() => this.props.navigation.openDrawer()}
+                            text={"Messages"} />
+                            
+                                <View style={styles.container}>
+                                    <BallIndicator color="#aaa" />
+                                </View>
+                    </Container>
+                    )
+        }
+        else{
         return(
-            <Container>
-                <ImageBackground style={{ width: '100%', height: '100%' }} source={require('../img/StreamImg/img_3.png')}></ImageBackground>
+            <Container style={{ flex: 1,backgroundColor:'#BFD0D670' }}>
+               
                 <DrawerMenuIcon
                         onPressMenuIcon={() => this.props.navigation.openDrawer()}
-                        text={this.t('Messages')} />
+                        text={"Messages"} />
+                        
+                            <View style={styles.container}>
+                                <FlatList
+                                data={this.state.publicMessages}
+                                renderItem={({item})=>
+                                <View style={styles.title}>
+                                    <Text style={{fontSize:20,fontStyle:'italic',color:'white'}}>{item.subject}</Text>
+                                    <View style={styles.message}>
+                                    <Text style={{fontSize:15,fontStyle:'italic'}}>{item.postText}</Text>
+                                </View>
+                                </View>
+                                }
+                                />
+                            </View>
+                        
             </Container>
         );
     }
-
+    }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'space-between',
+        
+    },
+    title:
+    {
+        alignSelf:'center',
+        marginTop:10,
+        width:'99%',
+        borderRadius:15,
+        backgroundColor: '#7154b8',
+        alignItems:'center'
+    },
+    message:
+    {
+        alignSelf:'center',
+        marginTop:10,
+        width:'100%',
+        borderBottomEndRadius:15,
+        borderBottomStartRadius:15,
+        backgroundColor: '#5E459B',
+        alignItems:'center'
+    }
+    
+})
 const mapDispatchToProps = (dispatch) => {
     return {
         //fetchAllMessages: (url) => dispatch(getData(url)),
