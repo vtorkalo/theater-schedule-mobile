47a51c4e987ded4957e9cefa01a10dd3e7c9bc4b
Shourek <34549142+Shourek@users.noreply.github.com>
2019-07-25 13:05:08 +0300

    Shourek lvneta 119 fix bugs (#101)
    * LVNETA-128 Fix bug in settings screen when you do some changes without saving them
    
    * Add some trim functions for right data fields validation
    
    * Change toast duration to 3000ms on settings screen for better visibility
    
    * Replace AsyncStorage.multiremove with AsyncStorage.clear on logout button

--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -19,6 +19,7 @@ import {
   Toast,
 } from "native-base";
 import Text from './Components/CustomText';
+import { NavigationEvents } from 'react-navigation';
 
 class SettingsScreen extends LocalizeComponent {
   static navigationOptions = ({ screenProps }) => {
@@ -30,9 +31,9 @@ class SettingsScreen extends LocalizeComponent {
 
   state = {
     settings: {
-      languageCode: this.props.settings.settings.languageCode,
-      doesNotify: this.props.settings.settings.doesNotify,
-      notificationFrequency: this.props.settings.settings.notificationFrequency
+      languageCode: '',
+      doesNotify: '',
+      notificationFrequency: ''
     }
   };
 
@@ -41,13 +42,15 @@ class SettingsScreen extends LocalizeComponent {
       Toast.show({
         text: this.t("Please try again"),
         buttonText: "Okay",
-        type: "danger"
+        type: "danger",
+        duration: 3000,
       });
     } else if (prevState.settings.loading && !this.props.settings.error) {
       Toast.show({
         text: this.t("Settings saved"),
         buttonText: "Okay",
-        type: "success"
+        type: "success",
+        duration: 3000,
       });
     }
   }
@@ -59,6 +62,24 @@ class SettingsScreen extends LocalizeComponent {
   render() {
     return (
       <Container>
+        <NavigationEvents
+          onWillFocus={() => {
+            this.setState({
+              settings: {
+                languageCode: this.props.settings.settings.languageCode,
+                doesNotify: this.props.settings.settings.doesNotify,
+                notificationFrequency: this.props.settings.settings.notificationFrequency
+              }});
+          }}
+          onDidBlur={() => {
+            this.setState({
+              settings: {
+                languageCode: '',
+                doesNotify: '',
+                notificationFrequency: ''
+              }});
+          }}
+        />
         <DrawerMenucIcon
           onPressMenuIcon={() => this.props.navigation.openDrawer()}
           text={this.t('Settings')} />
